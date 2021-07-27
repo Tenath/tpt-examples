74692f21fa8a3521b609633aa63f384672835ca9
Andrei Veeremaa <69631190+Tenath@users.noreply.github.com>
2021-07-27 21:51:58 +0300

    WpfNet

---
+++ ProgIntro/WpfNet/WpfNet/ChatServer.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WpfNet
+{
+    public class ChatServer : IDisposable
+    {
+        private MainWindow window;
+
+        private string bindPoint;
+        private ushort bindPort;
+
+        //private Socket listenSocket;
+        //private List<TcpClient> connections = new List<TcpClient>();
+        //private TcpListener listener;
+        private UdpClient client;
+        private Task<UdpReceiveResult> task;
+        //private Thread process_thread;
+
+        public ChatServer(MainWindow win, string iface, ushort port)
+        {
+            //Socket
+            window = win;
+            bindPoint = iface;
+            bindPort = port;
+            //Socket s = new Socket();
+            //s.Bind()
+
+            //IPEndPoint endpt = new IPEndPoint(IPAddress.Parse(iface), port);
+            //client = new UdpClient(endpt);
+            client = new UdpClient(port);
+        }
+
+        public void Update()
+        {
+            // Accept new connections
+            /*if (listener.Pending())
+            {
+                TcpClient cl = listener.AcceptTcpClient();
+                if (cl != null) connections.Add(cl);
+            }
+
+            foreach (TcpClient c in connections)
+            {
+                StreamReader rdr = new StreamReader(c.GetStream());
+                string line;
+                while ((line = rdr.ReadLine()) != null)
+                {
+                    window.WriteServer(line);
+                }
+            }*/
+            if(task==null)
+            {
+                task = client.ReceiveAsync();
+            }
+            else if (task.Status == TaskStatus.RanToCompletion)
+            {
+                IPEndPoint src = task.Result.RemoteEndPoint;
+                string buf = Encoding.UTF8.GetString(task.Result.Buffer);
+                string[] lines = buf.Split('\n');
+                foreach (string line in lines) window.WriteServer(src.Address.ToString(), line);
+
+                task.Dispose();
+                task = null;
+            }
+        }
+
+        public static List<string> GetLocalIPv4()
+        {
+            List<string> ipAddrList = new List<string>();
+            foreach (NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if ((item.NetworkInterfaceType == NetworkInterfaceType.Ethernet || item.NetworkInterfaceType == NetworkInterfaceType.Wireless80211) && item.OperationalStatus == OperationalStatus.Up)
+                {
+                    foreach (UnicastIPAddressInformation ip in item.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            ipAddrList.Add(ip.Address.ToString());
+                        }
+                    }
+                }
+            }
+
+            if (!ipAddrList.Contains("127.0.0.1")) ipAddrList.Insert(0, "127.0.0.1");
+            return ipAddrList;
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                //listenSocket.Shutdown(SocketShutdown.Both);
+                //listenSocket.Close();
+                client.Close();
+                if (task != null) task.Dispose();
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~ChatServer() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
---
+++ ProgIntro/WpfNet/WpfNet/MainWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfNet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> LocalIPs;
+
+        ChatServer server = null;
+        ChatClient client = null;
+
+        DispatcherTimer timer = new DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LocalIPs = ChatServer.GetLocalIPv4().ToList();
+
+            cboNetIf.DataContext = this;
+            cboNetIf.ItemsSource = LocalIPs;
+
+            if (LocalIPs.Count > 0) cboNetIf.SelectedIndex = 0;
+
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+            timer.Start();
+        }
+
+        private void BtnClientConnect_Click(object sender, RoutedEventArgs e)
+        {
+            // Проверка корректности входных данных
+            if(tbHost.Text.Length == 0)
+            {
+                MessageBox.Show("Не введён адрес для подключения!");
+                return;
+            }
+
+            if(!ushort.TryParse(tbPort.Text, out ushort port) || port==0)
+            {
+                MessageBox.Show("Неверно указан порт! Допустимый диапазон: 1-65535");
+                return;
+            }
+
+            if (tbMessage.Text.Length == 0)
+            {
+                MessageBox.Show("Пустое сообщение!");
+                return;
+            }
+
+            try
+            {
+                //IPEndPoint ep = new IPEndPoint(IPAddress.Parse(tbHost.Text), port);
+                //IPEndPoint lep = new IPEndPoint(IPAddress.Parse("172.16.2.24"), 50000);
+                using (UdpClient client = new UdpClient(tbHost.Text, port))
+                {
+                    byte[] data = Encoding.UTF8.GetBytes(tbMessage.Text);
+                    
+                    int sent = client.Send(data, data.Length);
+                }
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show($"Исключение: {exc.Message}\n{exc.StackTrace}");
+            }
+            
+        }
+
+        public void timer_Tick(object sender, EventArgs e)
+        {
+            if(server!=null) server.Update();
+        }
+
+        public void WriteServer(string src, string s)
+        {
+            DateTime now = DateTime.Now;
+            tbOutput.AppendText($"[{now.Hour:D2}:{now.Minute:D2}:{now.Second:D2}] ({src}) {s}\n");
+        }
+
+        private void BtnServe_Click(object sender, RoutedEventArgs e)
+        {
+            if(server!=null)
+            {
+                server.Dispose();
+                server = null;
+                btnServe.Content = "Запустить";
+                lbServerConnStatus.Content = "Выключен";
+                return;
+            }
+
+            if(LocalIPs.Count==0)
+            {
+                MessageBox.Show("Отсутствует адрес для привязки!");
+            }
+
+            if (!ushort.TryParse(tbServerPort.Text, out ushort port) || port == 0)
+            {
+                MessageBox.Show("Неверно указан порт! Допустимый диапазон: 1-65535");
+                return;
+            }
+
+            try
+            {
+                server = new ChatServer(this, cboNetIf.SelectedItem as string,port);
+
+                btnServe.Content = "Остановить";
+                lbServerConnStatus.Content = "Запущен";
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show($"Исключение: {exc.Message}");
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            tbOutput.Text = "";
+        }
+    }
+}
